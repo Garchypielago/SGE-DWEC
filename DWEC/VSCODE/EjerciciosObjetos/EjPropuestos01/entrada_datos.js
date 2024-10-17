09a826c12a385cd4b20abea0c524a5e8e9fb7961
garchy-home <jandro.garcia.sanchez@gmail.com>
2024-10-17 23:30:14 +0200

    17-10-24

--- DWEC/VSCODE/EjerciciosObjetos/EjPropuestos01/entrada_datos.js
+++ DWEC/VSCODE/EjerciciosObjetos/EjPropuestos01/entrada_datos.js
@@ -6,6 +6,7 @@
 // document.getElementById("demo").innerHTML = fruits.toString();
 
 const textoNuevo = document.getElementById("textoNuevo");
+const coincidencias = document.getElementById("coincidencias");
 
 function buscarPrimera() {
   let arrayTP = document.getElementById("textoPrincipal").value.split(" ");
@@ -39,6 +40,63 @@ function buscarTodas() {
     let nuevoTexto = "";
 
     while (arrayTP.indexOf(textoBuscar) != -1) {
+      arrayTP[arrayTP.indexOf(textoBuscar)] = textoReemplazar;
+    }
+
+    arrayTP.forEach(element => {
+      if (element != "." || element != "," || element != ";" || element != "!" || element != "¿" || element != ")" || element != "]" || element != "}" || element != "/") {
+        nuevoTexto += " " + element;
+      } else {
+        nuevoTexto += element;
+      }
+    });
+    textoNuevo.innerText = nuevoTexto;
+
+  } else {
+    textoNuevo.innerText = "No se ha encontrado esa palabra";
+  }
+}
+
+function contar() {
+  let arrayTP = document.getElementById("textoPrincipal").value.split(" ");
+  let textoBuscar = document.getElementById("textoBuscar").value;
+  let textoReemplazar = document.getElementById("textoReemplazar").value;
+
+  if (arrayTP.indexOf(textoBuscar) != -1) {
+    let nuevoTexto = "";
+    let contador = 0;
+
+    while (arrayTP.indexOf(textoBuscar) != -1) {
+      arrayTP[arrayTP.indexOf(textoBuscar)] = textoReemplazar;
+      contador++;
+    }
+
+    arrayTP.forEach(element => {
+      if (element != "." || element != "," || element != ";" || element != "!" || element != "¿" || element != ")" || element != "]" || element != "}" || element != "/") {
+        nuevoTexto += " " + element;
+      } else {
+        nuevoTexto += element;
+      }
+    });
+    textoNuevo.innerText = nuevoTexto;
+    coincidencias.innerText = "Ha habido " + contador + " coinciencias."
+
+  } else {
+    textoNuevo.innerText = "No se ha encontrado esa palabra";
+  }
+}
+
+function preguntar() {
+  let arrayTP = document.getElementById("textoPrincipal").value.split(" ");
+  let textoBuscar = document.getElementById("textoBuscar").value;
+  let textoReemplazar = document.getElementById("textoReemplazar").value;
+
+  if (arrayTP.indexOf(textoBuscar) != -1) {
+    let nuevoTexto = "";
+
+    while (arrayTP.indexOf(textoBuscar) != -1) {
+      // hacer metodo que enseñe el texto y pregunte, me da ereza ahora
+
       arrayTP[arrayTP.indexOf(textoBuscar)] = textoReemplazar;
     }
 
